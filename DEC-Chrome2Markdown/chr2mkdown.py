dacc1750c8338ee0017b7b617470262f174558ed
Dave Cohen <dcohen18@gmail.com>
2018-06-17 13:42:12 -0500

    Encapsulate and SRP

--- DEC-Chrome2Markdown/chr2mkdown.py
+++ DEC-Chrome2Markdown/chr2mkdown.py
@@ -33,73 +33,92 @@ replace:
 #Standard library:
 import os, sys, re, shutil
 #Local modules:
-from chr_config import myConfig
+import chr_config as config
 import date_append as DA
-from chr_backup import *
-from chr_path import *
+from file_utils import FileUtils
+from chr_path import getChromeJSON
 
-def deleter(f_str):
-    '''deletes type, url in f_str
-    '''
-    return f_str\
-    .replace('"type": "url",', '')
-    
-def replacer(f_str):
-    '''replaces html <a> tags
-    '''
-    return f_str                       \
-    .replace('<a', '</p>\n<a')      \
-    .replace('</a>', '</a>\n - ')
+fileUtils = FileUtils()
 
+class MarkdownCreator:
+    def __init__(self):
+        self.md_output = ''
 
+    def deleter(self, f_str):
+        '''deletes type, url in f_str
+        '''
+        return f_str\
+        .replace('"type": "url",', '')
+        
+    def replacer(self, f_str):
+        '''replaces html <a> tags
+        '''
+        return f_str \
+        .replace('<a', '</p>\n<a') \
+        .replace('</a>', '</a>\n - ')
 
-###CHECKS
-print('''
-DON'T FORGET:
-    -make sure that your mobile bookmarks have synced to desktop
-    -if you feel sure that you're ready, you can delete bookmarks after this backup.
-READY? hit Enter.''')
-yes = input('')
-###
+    def get_confirmation(self):
+        ###CHECKS
+        print('''
+        DON'T FORGET:
+            -make sure that your mobile bookmarks have synced to desktop
+            -if you feel sure that you're ready, you can delete bookmarks after this backup.
+        READY? hit Enter.
+        Not ready? Type any key then Enter to exit.''')
+        yes = input('')
+        if yes != '':
+            return False
+        return True
 
-#Backup bookmarks
-chrBackup()
+    def create_output_path(self):
+        md_oTemp = os.path.join(config.directories['bookmarksRootDir'], config.filenames['chr_md_file_prefix'])
+        self.md_output = DA.date_append(md_oTemp)
 
-#Markdown file - check output location
-md_oTemp = os.path.join(myConfig['myBackupDest'], myConfig['myMDfile'])
-md_output = DA.date_append(md_oTemp)
-fileExists(md_output) #raises error if exists
+    def write_bookmarks_to_file(self, _chrJSON):
+        output = open(self.md_output, 'w')
 
-# chrJSON = input('')
-# chrJSON = '/Users/davecohen/Library/Application Support/Google/Chrome/Default/Bookmarks'
+        with open(_chrJSON) as f:
+            for line in f:
+                line = line.strip()
+                line = self.deleter(line)
+                if line.startswith('"name"'):
+                    line = line.replace('"name": "', '* [')
+                    line = line.replace('",', ']')
+                    output.write(line)
+                elif line.startswith('"url"'):
+                    # if 'chrome-extension://' in line:
+                        # line = re.sub(r"chrome-extension://([a-z])+/suspended.html#uri=", "", line)
+                    line = re.sub(r"(chrome-extension://)(.+)(uri=)", "", line)
+                    line = line.replace('"url": "', '(')
+                    line_paren = line.replace('"', ')')
+                    line = line_paren + ' | ' + line_paren.replace('(', '[').replace(')', ']') + line_paren
+        #             print(self.replacer(line))
+                    output.write(line + '\n')
+                elif line == '"synced": {':
+                    output.write('\n\n## MOBILE BOOKMARKS\n\n')
 
-#Create Markdown from original "Bookmarks" file
-chrJSON = getChromeJSON(getChromePath())
+        output.close()
 
-output = open(md_output, 'w')
+        print('Output to:', self.md_output, '\n')
 
-with open(chrJSON) as f:
-    for line in f:
-        line = line.strip()
-        line = deleter(line)
-        if line.startswith('"name"'):
-            line = line.replace('"name": "', '* [')
-            line = line.replace('",', ']')
-            output.write(line)
-        elif line.startswith('"url"'):
-            # if 'chrome-extension://' in line:
-                # line = re.sub(r"chrome-extension://([a-z])+/suspended.html#uri=", "", line)
-            line = re.sub(r"(chrome-extension://)(.+)(uri=)", "", line)
-            line = line.replace('"url": "', '(')
-            line_paren = line.replace('"', ')')
-            line = line_paren + ' | ' + line_paren.replace('(', '[').replace(')', ']') + line_paren
-#             print(replacer(line))
-            output.write(line + '\n')
-        elif line == '"synced": {':
-            output.write('\n\n## MOBILE BOOKMARKS\n\n')
+    def run_script(self):
+        if not self.get_confirmation():
+            print('Exiting.')
+            return
 
-output.close()
+        self.create_output_path()
 
-print('Output to:', md_output, '\n')
+        #Markdown file - check output location
+        fileUtils.fileExists(self.md_output) #raises error if exists
 
-# print('done') #debug
+        # chrJSON = input('')
+        # chrJSON = '/Users/davecohen/Library/Application Support/Google/Chrome/Default/Bookmarks'
+
+        #Create Markdown from original "Bookmarks" file
+        chrJSON = getChromeJSON()
+
+        print('chrJSON', chrJSON)#debug
+
+        self.write_bookmarks_to_file(chrJSON)
+
+        # print('done') #debug
