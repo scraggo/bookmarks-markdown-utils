81df7407ba63eb8658b92075c76f388e089e5a00
Dave Cohen <dcohen18@gmail.com>
2018-06-23 22:56:40 -0500

    Change description

--- chrome_to_markdown/main.py
+++ chrome_to_markdown/main.py
@@ -2,29 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on Sat Jun 16 2018
-
-@author: davecohen
-
 Title: Convert Chrome Bookmarks JSON file to Markdown
     and backup Chrome Bookmarks
-
-Output Format:
-* [UPS: Register](https://www.ups.com/one-to-one/login) | [https://www.ups.com/one-to-one/login](https://www.ups.com/one-to-one/login)
-
-Input Example:
-"name": "UPS: Register",
-"url": "chrome-extension://klbibkeccnjlkjkiokjodocebajanakg/suspended.html#uri=https://www.ups.com/one-to-one/login"
-
-parsing:
-delete: "type": "url",
-replace: 
-"name": with [ ]
-"url": with ( )
-
 """
 
-#Standard library:
 import os, sys, subprocess
 
 from chr_backup import chrBackup
@@ -47,21 +28,16 @@ def test_things():
   print(path, file_name)
 
 def main():
-
   chrBackup()  #Backup bookmarks
-
   md_creator = MarkdownCreator()
   md_creator.run_script()
   new_file_path = md_creator.md_output
   # path, file_name = os.path.split(new_file_path)
   # print('Split up the path and file name:\n', path, file_name)
   # move_files(new_file_path)
-  subprocess.run(['node', config['node_scripts']['deleteLeadingText'], new_file_path])
+  subprocess.run(['python', '../delete_leading_text/delete_leading_text.py', new_file_path])
 
 
 if __name__ == '__main__':
   # test_things()
   main()
-
-
-
